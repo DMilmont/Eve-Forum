0ab97b07b6507e3ae59213c6dd1a2d7857d4bd91
David Milmont <davidmilmont@gmail.com>
2016-04-05 10:05:58 -0600

    Update eve.php

--- eve.php
+++ eve.php
@@ -15,13 +15,13 @@ function httprequest($url, $postvars = NULL){
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 	curl_setopt($ch, CURLOPT_HEADER, 0);
 	curl_setopt($ch, CURLOPT_COOKIEJAR, "cookie.txt");
-    curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie.txt");
-    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
-    curl_setopt($ch, CURLOPT_TIMEOUT, 40);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    	curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie.txt");
+    	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
+    	curl_setopt($ch, CURLOPT_TIMEOUT, 40);
+    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 	//curl_setopt($ch, CURLOPT_VERBOSE, TRUE);
 	
 	if($postvars){
@@ -125,7 +125,5 @@ while(TRUE){
 	}
 	
 }
-
-
 
 ?>
